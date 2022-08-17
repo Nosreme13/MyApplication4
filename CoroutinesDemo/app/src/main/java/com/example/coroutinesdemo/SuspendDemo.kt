4e829d6ae1d126f89fba4f8d85583517ab16ba01
Nosreme <gorianosreme581@gmail.com>
2022-08-17 17:25:27 +0800

    Initial Commit

---
+++ CoroutinesDemo/app/src/main/java/com/example/coroutinesdemo/SuspendDemo.kt
@@ -0,0 +1,11 @@
+package com.example.coroutinesdemo
+
+class SuspendDemo {
+    private fun firstFunction(){
+        //some code
+
+    }
+    private suspend fun secondFunction(){
+
+    }
+}
---
+++ ViewModelScopeDemo/app/src/main/java/com/example/viewmodelscopedemo/model/User.kt
@@ -0,0 +1,5 @@
+package com.example.viewmodelscopedemo.model
+
+class User(val id : Int, val name : String) {
+
+}
